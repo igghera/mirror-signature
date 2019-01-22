a5f2bccbedecb0dca6d4eadd314c64e11e4bacbb
Andrea Gherardi <a.gherardi85@gmail.com>
2019-01-22 11:36:07 +0100

    Updates

---
+++ precache-manifest.86579176d91468ec23cc40819c8d667c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e059b78c6e87f53e6966",
+    "url": "/mirror-signature/static/css/main.add7e8e6.chunk.css"
+  },
+  {
+    "revision": "e059b78c6e87f53e6966",
+    "url": "/mirror-signature/static/js/main.e059b78c.chunk.js"
+  },
+  {
+    "revision": "4bdba03407a4aca5e71e",
+    "url": "/mirror-signature/static/js/1.4bdba034.chunk.js"
+  },
+  {
+    "revision": "e6d480e6a0f2592c3781",
+    "url": "/mirror-signature/static/js/runtime~main.e6d480e6.js"
+  },
+  {
+    "revision": "4ffb345d735c13ce6b25d1c7c198f624",
+    "url": "/mirror-signature/index.html"
+  }
+];
