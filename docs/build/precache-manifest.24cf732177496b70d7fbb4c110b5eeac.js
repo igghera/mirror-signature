0f7cc0b5a2dbe4b353ec843f7c40226b8e06d903
Andrea Gherardi <andrea@mirrorprod.com>
2021-06-03 12:51:20 +0200

    Update firma

---
+++ docs/build/precache-manifest.24cf732177496b70d7fbb4c110b5eeac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3ede6d103eff48be104",
+    "url": "/mirror-signature/static/css/main.add7e8e6.chunk.css"
+  },
+  {
+    "revision": "e3ede6d103eff48be104",
+    "url": "/mirror-signature/static/js/main.e3ede6d1.chunk.js"
+  },
+  {
+    "revision": "4bdba03407a4aca5e71e",
+    "url": "/mirror-signature/static/js/1.4bdba034.chunk.js"
+  },
+  {
+    "revision": "e6d480e6a0f2592c3781",
+    "url": "/mirror-signature/static/js/runtime~main.e6d480e6.js"
+  },
+  {
+    "revision": "25a84aa9a918eb1af38ec45fdacc8164",
+    "url": "/mirror-signature/index.html"
+  }
+];
