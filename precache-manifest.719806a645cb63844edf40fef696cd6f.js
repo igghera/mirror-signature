c5f1db7fdcf87bd80aca0d609533a7c25f7a60a8
Andrea Gherardi <andreagherardi@MBP-di-Andrea.homenet.telecomitalia.it>
2021-06-15 15:31:54 +0200

    Updates

---
+++ precache-manifest.719806a645cb63844edf40fef696cd6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af9a5235607f56f71e73",
+    "url": "/mirror-signature/static/css/main.50d2fc4f.chunk.css"
+  },
+  {
+    "revision": "af9a5235607f56f71e73",
+    "url": "/mirror-signature/static/js/main.af9a5235.chunk.js"
+  },
+  {
+    "revision": "4bdba03407a4aca5e71e",
+    "url": "/mirror-signature/static/js/1.4bdba034.chunk.js"
+  },
+  {
+    "revision": "e6d480e6a0f2592c3781",
+    "url": "/mirror-signature/static/js/runtime~main.e6d480e6.js"
+  },
+  {
+    "revision": "545aae6802817390f900db3cabc988a6",
+    "url": "/mirror-signature/index.html"
+  }
+];
